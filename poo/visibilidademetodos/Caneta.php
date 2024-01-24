6eb1e918344a179c58cd5035a373c50f8372c2e3
anabel <careca_maia18@hotmail.com>
2024-01-24 00:17:11 -0400

    add novos arquivos

---
+++ poo/visibilidademetodos/Caneta.php
@@ -0,0 +1,22 @@
+<?php 
+class Caneta{
+    public $modelo;
+    public $cor;
+    private $ponta;
+    protected $carga;
+    protected $tampada;
+
+    public function rabiscar(){
+
+      
+    }
+    private function tampar(){
+
+
+    }
+    private function destampar(){
+
+    }
+}
+
+?>
