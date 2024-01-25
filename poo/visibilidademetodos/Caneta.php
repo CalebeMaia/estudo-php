7f74f0cf7599b4f04e12a36792c6226a273420e2
anabel <careca_maia18@hotmail.com>
2024-01-25 00:25:59 -0400

    arquivo de teste

--- poo/visibilidademetodos/Caneta.php
+++ poo/visibilidademetodos/Caneta.php
@@ -7,15 +7,19 @@ class Caneta{
     protected $tampada;
 
     public function rabiscar(){
-
+        if($this->tampada==true;){
+            echo "erro nao posso rabiscar";
+        }else{
+            echo "estou rabiscando...";
+        }
       
     }
     private function tampar(){
-
+        $this->tampada=true;
 
     }
     private function destampar(){
-
+        $this->tampada=false;
     }
 }
 
