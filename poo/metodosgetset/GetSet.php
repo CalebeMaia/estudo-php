7b49798fa0a5bc9418020a191511cc9d4a71eb18
anabel <careca_maia18@hotmail.com>
2024-01-25 23:31:55 -0400

    metodos get set

--- poo/metodosgetset/GetSet.php
+++ poo/metodosgetset/GetSet.php
@@ -1,8 +1,24 @@
 <?php 
     class Caneta{
-      public $modelo;
+      private $modelo;
+      private $cor;
       private $ponta;
-      
+      private $tampada;
+
+      public function __construct(){     //o metodo construtor serve para construir um obejto sempre com esses atributos e como se fosse uma forma
+                                        // nos contrutores é comum colocar o mesmo nome da classe
+        $this->cor="azul";
+        $this->tampada=true;
+        $this->ponta="0.7";
+        $this->modelo="super caneta";
+
+
+      }
+
+      public function tampar(){
+
+      }
+  
       public function getModelo(){
         return $this->modelo;
 
@@ -10,11 +26,23 @@
       public function setModelo($m){
         $this->modelo=$m;
       }
-      public function getPonta(){
+      public function getCor(){
+        return $this->cor;
+      }
+      public function setCor($c){
+        $this->cor=$c;
+      }
+      public function getPonta(){ 
         return $this->ponta;
       }
       public function setPonta($p){
         $this->ponta=$p;
+      }
+      public function getTampar(){
+          return $this->tampada;
+      }
+      public function setTampada($t){
+          $this->tampada=$t;
       }
 
     }
--- poo/metodosgetset/index.php
+++ poo/metodosgetset/index.php
@@ -6,11 +6,28 @@
     <title>Document</title>
 </head>
 <body>
+    <pre>
     <?php 
         require_once 'GetSet.php';
         $c1=new Caneta();
+        $c1->setModelo("fit");
+        $c1->setPonta(0.5);
+        print_r($c1);
+
+        $c2=new Caneta();
+        $c2->setModelo("caneta top");
+        $c2->setPonta(0.7);
+        print_r($c2);
+
+        $c3=new Caneta();
+            print_r($c3);
+
+            $c4=new Caneta();
+            print_r($c4);
+        
+
     
     ?>
-    
+    </pre>
 </body>
 </html>
