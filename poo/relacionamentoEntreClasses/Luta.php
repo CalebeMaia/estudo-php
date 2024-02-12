a533bf8a0892a3a3255b948193dace55f7aa8f6c
anabel <careca_maia18@hotmail.com>
2024-02-12 11:03:13 -0400

    testando

---
+++ poo/relacionamentoEntreClasses/Luta.php
@@ -0,0 +1,77 @@
+<?php 
+    require_once "Lutador.php";
+    class Luta{
+        private $desafiado;
+        private $desafiante;
+        private $rounds;
+        private $aprovada;
+
+        
+        public function marcarLuta($l1,$l2){
+            if($l1->getCategoria()===$l2->getCategoria()&&($l1!=$l2)){
+                $this->aprovada=true;
+                $this->desafiado=$l1;
+                $this->desafiante=$l2;
+            }else{
+                $this->aprovada=false;
+                $this->desafiado=null;
+                $this->desafiante=null;
+            }
+        }
+        public function lutar(){
+            if($this->aprovada){
+             $this->desafiado->apresentar();
+             $this->desafiante->apresentar();
+             $vencedor=rand(0,2);   
+             switch($vencedor){
+                case 0: //empate
+                    echo "<p> Empate </p>";
+                    $this->desafiado->empatarLuta();
+                    $this->desafiante->empatarLuta();
+                    break;
+                case 1: // desafiado vence
+                    echo " <p> ".$this->desafiado->getNome()." Venceu ";
+                    $this->desafiado->ganharLuta();
+                    $this->desafiante->perderLuta();
+                    break;
+                case 2: //desafiante vence
+                    echo "<p>".$this->desafiante->getNome(). "Venceu";
+                    $this->desafiado->perderLuta();
+                    $this->desafiante->ganharLuta();
+                    break;
+             }
+
+            }else{
+                echo "<p> não pode acontecer </p>";
+            }
+        }
+        //métodos especiais
+        public function getDesafiado(){
+            return $this->desafiado;
+        }
+        public function setDesafiado($de){
+            $this->desafiado=$de;
+        }
+        public function getDesafiante(){
+            return $this->desafiante;
+        }
+        public function setDesafiante($desafiante){
+            $this->desafiante=$desafiante;
+        }
+        public function getRounds(){
+            return $this->rounds;
+        }
+        public function setRounds($ro){
+            $this->rounds=$ro;
+        }
+        public function getAprovada(){
+            return $this->aprovada;
+        }
+        public function setAprovada($ap){
+            $this->aprovada=$ap;
+        }
+
+    }
+
+
+?>
--- poo/relacionamentoEntreClasses/index.php
+++ poo/relacionamentoEntreClasses/index.php
@@ -9,10 +9,19 @@
     <pre>
     <?php 
         require_once "Lutador.php";
-        $l=new Lutador("Calebe","Brasil",36,1.75,10,3,1,83);
+        require_once "Luta.php";
+        //$l=new Lutador("Calebe","Brasil",36,1.75,10,3,1,83);
         //print_r($l);
-        $l->apresentar();
-
+        $l=array();
+        $l[0]=new Lutador("Calebe","Brasil",36,1.75,10,3,1,83);
+        $l[1]=new Lutador ("Pony","Roraima",13,1.62,3,1,1,82);
+        $l[2]=new Lutador ("Danie","Roraima",40,1.62,3,1,1,31);
+        $l[3]=new Lutador ("Sauri","Roraima",12,1.62,3,1,1,33);
+         $teste = new Luta();
+         $teste->marcarLuta($l[1],$l[0]);
+         $teste->lutar();
+         $l[0]->status();
+         $l[1]->status();
     ?>
     </pre>
 </body>
