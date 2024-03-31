feba8d7c7f65c5dda6a4027d3fd6818ff365e306
anabel <careca_maia18@hotmail.com>
2024-03-31 18:55:47 -0400

    continuando

---
+++ mvc/Model.php
@@ -0,0 +1,18 @@
+<?php 
+//este código tem objetivo de treinar através da repetição o uso a estrutura MVC
+class Model{
+    public $string;
+
+    public function __construct(){
+        $this->string="ola mundo";
+    }
+    public function get_string(){
+        return $this->string;
+    }
+    
+    
+    
+}
+
+
+?>
